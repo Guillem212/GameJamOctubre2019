4e5e067eccd1ab890abf357bf979e51647811788
Alvaro :) <al361691@uji.es>
2019-10-19 10:55:22 +0200

    Commit to master

---
+++ Assets/Scenes/Alvaro/Scripts/PlayerCanvasBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCanvasBehavior : MonoBehaviour
+{
+    [SerializeField] GameObject AButton;
+
+    void ShowAButtonOnCanvas(bool state)
+    {
+        AButton.SetActive(state);
+    }
+}
