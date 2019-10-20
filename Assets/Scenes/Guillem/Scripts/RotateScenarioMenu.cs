17ac47d43d68c2d708907c7519104c8b9498af3b
Guillem Ortega <guillem212@hotmail.es>
2019-10-20 17:43:12 +0200

    menu

---
+++ Assets/Scenes/Guillem/Scripts/RotateScenarioMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScenarioMenu : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.Rotate(0f, 0.2f, 0f, Space.Self);
+    }
+}
