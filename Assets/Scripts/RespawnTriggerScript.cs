e3bdd027489453d9cd76bf96b6d510d8d3cf74c8
Guillem Ortega <guillem212@hotmail.es>
2019-12-19 17:05:33 +0100

    Muchas cosas
    -YA HAY RESPAWN CUANDO TE CAES
    -La nueva hierba está implementada
    -Se ha eliminado la mierda de las nubes
    -Se han mejorado iluminacion y particulas un poquiño

---
+++ Assets/Scripts/RespawnTriggerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnTriggerScript : MonoBehaviour
+{
+    public Player[] playerScript;
+
+    private void Start() {
+        playerScript = new Player[2];
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.gameObject.CompareTag("Player")){
+            if(playerScript[0] != null){
+                playerScript[1] = other.gameObject.GetComponent<Player>();
+                playerScript[1].GoToSpawn();
+                playerScript[1] = null;
+            }
+            else{
+                playerScript[0] = other.gameObject.GetComponent<Player>();
+                playerScript[0].GoToSpawn();
+                playerScript[0] = null;
+            }
+        }
+    }
+}
